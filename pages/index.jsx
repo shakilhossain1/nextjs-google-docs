ed43757b3ac8f4c03aab60a6aa09e5854a0a3e73
Shakil Hossain <sh9743441@gmail.com>
2021-07-19 18:31:13 +0600

    build the header sectino

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,9 @@
-import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <div>
-        Hello there
-      </div>
+    <div className="">
+      <Header />
     </div>
   )
 }
